ffff1d3a44b13269d637028eb9906a68c5a32bd0
Julian <j.vandennieuwenhuizen@student.utwente.nl>
2023-03-22 15:42:37 +0100

    xml builder

--- src/main/java/bolk_app/services/XMLBuilder.java
+++ src/main/java/bolk_app/services/XMLBuilder.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class XMLBuilder {
-
+    private int numberofunits = 0;
     public String build() {
 
         return null;
     }
+    //private int numberofunits (Number of pallets/boxes)
+    //receive string, convert to xml and save in a file
+    //use adress of bolk, get adress for recipient from database
+    //loop through units with numberofunits
+    //split string on a character
 }
