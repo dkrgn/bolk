1e968502a6f1c067fcded89f7573e5bcc7612c64
Julian <j.vandennieuwenhuizen@student.utwente.nl>
2023-03-27 01:01:32 +0200

    xml builder changes

--- src/main/java/bolk_app/services/XMLBuilder.java
+++ src/main/java/bolk_app/services/XMLBuilder.java
@@ -6,6 +6,42 @@ import org.springframework.stereotype.Service;
 public class XMLBuilder {
     private int numberofunits = 0;
     public String build() {
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <PickupAddress>
+                <Name>Bolk Transport B.V.</Name>
+                <Street>Zuidelijke Havenweg</Street>
+                <Nr>4</Nr>
+                <Zipcode>7554RR</Zipcode>
+                <City>Hengelo</City>
+                <Country>NL</Country>
+                <Date>java.time.LocalDate.now()</Date>
+        <PickupAddress/>
+
+        <DeliveryAdress>
+                <Name> </Name>
+                <Street> </Street>
+                <Nr> </Nr>
+                <Zipcode> </Zipcode>
+                <City> </City>
+                <Country> </Country>
+                <Date>java.time.LocalDate.now()</Date>
+        <PickupAddress/>
+
+        <Goods_Details>
+            <Goods_Detail>
+                <PackageQuantity> </PackageQuantity>
+                <PackageUnitCode> </PackageUnitCode> //Pallettype / Box
+                <Goods_Weight></Goods_Weight> //in kg
+                <Collo Detail>
+                    <Collo_Length> </Collo_Length> //in cm
+                    <Collo_Width> </Collo_Width> //in cm
+                    <Collo_Heigth> </Collo_Heigth> //in cm
+                    <Collo_Weigth> </Collo_Weigth> //in kg, same as Goods_Weight
+                    <Collo_PalletUnitCode> </Collo_PalletsUnitCode>
+                </Collo Detail>
+            </Goods_Detail>
+        </Goods_Details>
+
 
         return null;
     }
